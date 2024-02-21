b8772ea2fb8c20b1314b0b37ec6d5f7bfc40ba19
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-02-21 22:01:02 +0100

    interactive-rating

--- interactive-rating/src/App.jsx
+++ interactive-rating/src/App.jsx
@@ -7,24 +7,24 @@ import './App.css'
 function App() {
   
   const [active,setActive] = useState(false)
-  
-  const handleClick = () =>{
-    setActive(!active)
-  }
-  
-
-  
   const [submit,setSubmit] = useState (false)
+  const [rating,setRating] = useState(null)
+  
+  const handleClick = (number) =>{
+    setActive(true);
+    setRating(number)
+  };
+   
   const handleSubmitclick = () => {
     const thankYou = document.querySelector(".thank-you");
     const container = document.querySelector(".container");
-    if(handleClick !==0){
+    
+    if(active){
     container.style.display = "none"
     thankYou.style.display = "flex"
-  }
     setSubmit(!submit)
-  
-
+  }
+   
   } 
   
    return (
@@ -43,24 +43,26 @@ function App() {
         request. All feedback is appreciated to help us improve our offering! </p>
     </span>
     <div className="buttons" >
-      <button onClick={handleClick}>1</button>
-      <button onClick={handleClick}>2</button>
-      <button onClick={handleClick}>3</button>
-      <button onClick={handleClick}>4</button>
-      <button onClick={handleClick}>5</button>
+      {[1,2,3,4,5].map((number) =>(
+        <button key={number} onClick = {() => handleClick(number)}>
+        {number}
+        </button>
+      ))}
     </div>
     <button className='submit' onClick={handleSubmitclick}>SUBMIT</button>
+    </div>
   
-  </div> 
+    
     <div className='thank-you' style={{display:"none"}} >
       <img src='illustration-thank-you.svg'></img>
-      <p className='rating'>You selected {setActive} out of 5</p>
+      <p className='rating'>You selected {rating} out of 5</p>
       <span>
       <h1>Thank you!</h1>
       <p>We appreciate you taking the time to give a rating. If you ever need
       more support, don’t hesitate to get in touch!</p>
     </span>
     </div>
+     
   </main>
   ) 
 }
